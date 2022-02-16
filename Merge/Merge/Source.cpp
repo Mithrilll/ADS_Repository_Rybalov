933bd27e3b8fca3b893843f3c8410153e5d1e5cb
mifril <rybalovalex03@gmail.com>
2022-02-16 12:18:38 +0700

    Added function createFileWithRandomNumbers
    Function creates file with numbersCount in folder with  source.cpp

--- Merge/Merge/Source.cpp
+++ Merge/Merge/Source.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <random>
+#include <time.h>
+
+bool createFileWithRandomNumbers(const std::string& filename, const int numbersCount, const int maxNumberValue)
+{
+	srand(time(0));
+
+	std::ofstream fout;
+	fout.open(filename);
+	if (!fout.is_open())
+	{
+		// "Cannot open file";
+		return false;
+	}
+
+	for (int i = 0; i < numbersCount; i++)
+		fout << rand() % (maxNumberValue + 1) << " ";
+
+	fout.close();
+
+	return true;
+}
+
+
 
 int main()
 {
