9355cc9c966f300437a32db09ceb0576b064f349
mifril <rybalovalex03@gmail.com>
2022-09-12 11:20:45 +0700

    Implemented actions in main to test, balanced search tree and created report

--- Balanced_Binary_Tree/Balanced_Binary_Tree/main.cpp
+++ Balanced_Binary_Tree/Balanced_Binary_Tree/main.cpp
@@ -7,7 +7,7 @@ int testBalancedSearchTree(balanced_search_tree& tree, const int size)
 {
 	if (tree.countNodes() != size) 
 	{
-		std::cout << "!S" << std::endl;
+		std::cout << "Wrong size!" << std::endl;
 		return -1;
 	}
 
@@ -37,8 +37,7 @@ int testBalancedSearchTree(balanced_search_tree& tree, const int size)
 
 			if (secondChildKey >= 0 && !(firstChildKey <= rootKey && rootKey <= secondChildKey)) 
 			{
-				subTree.printVertical();
-				std::cout << "!=" << std::endl;
+				std::cout << "Not search tree!" << std::endl;
 				std::cout << std::endl;
 
 				return -2;
@@ -46,8 +45,7 @@ int testBalancedSearchTree(balanced_search_tree& tree, const int size)
 
 			if (!subTree.isBalanced()) 
 			{
-				subTree.printVertical();
-				std::cout << "!B" << std::endl;
+				std::cout << "Not balanced!" << std::endl;
 				std::cout << std::endl;
 
 				return -3;
@@ -55,7 +53,7 @@ int testBalancedSearchTree(balanced_search_tree& tree, const int size)
 		}
 	}
 
-	std::cout << "OK" << std::endl;
+	std::cout << "Test passed!" << std::endl;
 	return 0;
 }
 
@@ -76,7 +74,7 @@ int testBalancedSearchTree(int size)
 
 	while (nodesKeys.size()) 
 	{
-		index = rand() % nodesKeys.size();
+		/*index = rand() % nodesKeys.size();
 		nodesKeys.erase(nodesKeys.begin() + index);
 		std::cout << "index: " << index << std::endl;
 		balancedTree.printVertical();
@@ -90,7 +88,7 @@ int testBalancedSearchTree(int size)
 		{
 			failedCase = 2;
 			break;
-		}
+		}*/
 
 		if (nodesKeys.size()) 
 		{
@@ -131,16 +129,20 @@ int testBalancedSearchTree(int size)
 
 int testBalancedAsSearchTree(search_tree& tree, const int size)
 {
-	if (tree.countNodes() != size) {
+	if (tree.countNodes() != size)
+	{
+		std::cout << "Wrong size!" << std::endl;
 		return -1;
 	}
 
 	std::vector<int> nodesKeys = tree.round();
 
-	for (int i = size - 1; i >= 0; --i) {
+	for (int i = size - 1; i >= 0; --i)
+	{
 		binary_tree subTree = tree.copySubTree(i);
 		int subTreeSize = subTree.countNodes();
-		if (subTreeSize > 1) {
+		if (subTreeSize > 1)
+		{
 			int rootKey = subTree.key_by_index(0);
 			int rootLevel = subTree.level(rootKey);
 
@@ -148,19 +150,34 @@ int testBalancedAsSearchTree(search_tree& tree, const int size)
 			int childsLevel = subTree.level(firstChildKey);
 
 			int secondChildKey = -1;
-			if (subTreeSize > 2) {
+			if (subTreeSize > 2)
+			{
 				secondChildKey = subTree.key_by_index(2);
-				if (subTree.level(secondChildKey) != childsLevel) {
+				if (subTree.level(secondChildKey) != childsLevel)
+				{
 					secondChildKey = -1;
 				}
 			}
 
-			if (secondChildKey >= 0 && !(firstChildKey < rootKey && rootKey <= secondChildKey)) {
+			if (secondChildKey >= 0 && !(firstChildKey <= rootKey && rootKey <= secondChildKey))
+			{
+				std::cout << "Not search tree!" << std::endl;
+				std::cout << std::endl;
+
 				return -2;
+			}
+
+			if (!subTree.isBalanced())
+			{
+				std::cout << "Not balanced!" << std::endl;
+				std::cout << std::endl;
+
+				return -3;
 			}
 		}
 	}
 
+	std::cout << "Test passed!" << std::endl;
 	return 0;
 }
 
@@ -171,7 +188,8 @@ bool testBalancedAsSearchTree(int size)
 	search_tree& balancedTreeRef = balancedTree;
 	std::vector<int> nodesKeys;
 
-	for (int i = 0; i < size; ++i) {
+	for (int i = 0; i < size; ++i)
+	{
 		int key = rand() % 10;
 		balancedTreeRef.add(key);
 		nodesKeys.push_back(key);
@@ -179,62 +197,77 @@ bool testBalancedAsSearchTree(int size)
 
 	int index, key;
 
-	while (nodesKeys.size()) {
-		index = rand() % nodesKeys.size();
+	while (nodesKeys.size())
+	{
+		/*index = rand() % nodesKeys.size();
 		nodesKeys.erase(nodesKeys.begin() + index);
-		if (!balancedTreeRef.find_and_erase_by_index(index)) {
+		std::cout << "index: " << index << std::endl;
+		balancedTree.printVertical();
+		if (!balancedTree.find_and_erase_by_index(index))
+		{
 			failedCase = 1;
 			break;
 		}
 
-		if (testBalancedAsSearchTree(balancedTreeRef, nodesKeys.size())) {
+		if (testBalancedSearchTree(balancedTree, nodesKeys.size()))
+		{
 			failedCase = 2;
 			break;
-		}
+		}*/
 
-		if (nodesKeys.size()) {
+		if (nodesKeys.size())
+		{
 			index = rand() % nodesKeys.size();
 			key = nodesKeys[index];
 			nodesKeys.erase(nodesKeys.begin() + index);
-			if (!balancedTreeRef.find_and_erase_by_key(key)) {
+			std::cout << "key: " << key << std::endl;
+			balancedTreeRef.printVertical();
+			if (!balancedTreeRef.find_and_erase_by_key(key))
+			{
 				failedCase = 3;
 				break;
 			}
 
-			if (testBalancedAsSearchTree(balancedTreeRef, nodesKeys.size())) {
+			if (testBalancedAsSearchTree(balancedTreeRef, nodesKeys.size()))
+			{
 				failedCase = 4;
 				break;
 			}
 		}
 	}
 
-	if (failedCase || !balancedTreeRef.isEmpty()) {
-		if (!failedCase) {
+	if (failedCase || !balancedTree.isEmpty())
+	{
+		if (!failedCase)
+		{
 			failedCase = -1;
 		}
 
-		std::cout << size << " " << balancedTreeRef.countNodes() << std::endl;
-		balancedTreeRef.printVertical();
+		std::cout << size << " " << balancedTree.countNodes() << std::endl;
+		balancedTree.printVertical();
 		std::cout << std::endl;
 		std::cout << std::endl;
-
-
 	}
-	return (failedCase == 0);
+
+	return failedCase;
 }
 
 int testBalancedAsBinaryTree(binary_tree& tree, const int size)
 {
-	if (tree.countNodes() != size) {
+	if (tree.countNodes() != size)
+	{
+		std::cout << "Wrong size!" << std::endl;
 		return -1;
 	}
 
 	std::vector<int> nodesKeys = tree.round();
 
-	for (int i = size - 1; i >= 0; --i) {
+	for (int i = size - 1; i >= 0; --i)
+	{
 		binary_tree subTree = tree.copySubTree(i);
 		int subTreeSize = subTree.countNodes();
-		if (subTreeSize > 1) {
+		if (subTreeSize > 1)
+		{
 			int rootKey = subTree.key_by_index(0);
 			int rootLevel = subTree.level(rootKey);
 
@@ -242,19 +275,34 @@ int testBalancedAsBinaryTree(binary_tree& tree, const int size)
 			int childsLevel = subTree.level(firstChildKey);
 
 			int secondChildKey = -1;
-			if (subTreeSize > 2) {
+			if (subTreeSize > 2)
+			{
 				secondChildKey = subTree.key_by_index(2);
-				if (subTree.level(secondChildKey) != childsLevel) {
+				if (subTree.level(secondChildKey) != childsLevel)
+				{
 					secondChildKey = -1;
 				}
 			}
 
-			if (secondChildKey >= 0 && !(firstChildKey < rootKey && rootKey <= secondChildKey)) {
+			if (secondChildKey >= 0 && !(firstChildKey <= rootKey && rootKey <= secondChildKey))
+			{
+				std::cout << "Not search tree!" << std::endl;
+				std::cout << std::endl;
+
 				return -2;
+			}
+
+			if (!subTree.isBalanced())
+			{
+				std::cout << "Not balanced!" << std::endl;
+				std::cout << std::endl;
+
+				return -3;
 			}
 		}
 	}
 
+	std::cout << "Test passed!" << std::endl;
 	return 0;
 }
 
@@ -265,7 +313,8 @@ bool testBalancedAsBinaryTree(int size)
 	binary_tree& balancedTreeRef = balancedTree;
 	std::vector<int> nodesKeys;
 
-	for (int i = 0; i < size; ++i) {
+	for (int i = 0; i < size; ++i)
+	{
 		int key = rand() % 10;
 		balancedTreeRef.add(key);
 		nodesKeys.push_back(key);
@@ -273,56 +322,88 @@ bool testBalancedAsBinaryTree(int size)
 
 	int index, key;
 
-	while (nodesKeys.size()) {
-		index = rand() % nodesKeys.size();
+	while (nodesKeys.size())
+	{
+		/*index = rand() % nodesKeys.size();
 		nodesKeys.erase(nodesKeys.begin() + index);
-		if (!balancedTreeRef.find_and_erase_by_index(index)) {
+		std::cout << "index: " << index << std::endl;
+		balancedTree.printVertical();
+		if (!balancedTree.find_and_erase_by_index(index))
+		{
 			failedCase = 1;
 			break;
 		}
 
-		if (testBalancedAsBinaryTree(balancedTreeRef, nodesKeys.size())) {
+		if (testBalancedSearchTree(balancedTree, nodesKeys.size()))
+		{
 			failedCase = 2;
 			break;
-		}
+		}*/
 
-		if (nodesKeys.size()) {
+		if (nodesKeys.size())
+		{
 			index = rand() % nodesKeys.size();
 			key = nodesKeys[index];
 			nodesKeys.erase(nodesKeys.begin() + index);
-			if (!balancedTreeRef.find_and_erase_by_key(key)) {
+			std::cout << "key: " << key << std::endl;
+			balancedTreeRef.printVertical();
+			if (!balancedTreeRef.find_and_erase_by_key(key))
+			{
 				failedCase = 3;
 				break;
 			}
 
-			if (testBalancedAsBinaryTree(balancedTreeRef, nodesKeys.size())) {
+			if (testBalancedAsBinaryTree(balancedTreeRef, nodesKeys.size()))
+			{
 				failedCase = 4;
 				break;
 			}
 		}
 	}
 
-	if (failedCase || !balancedTreeRef.isEmpty()) {
-		if (!failedCase) {
+	if (failedCase || !balancedTree.isEmpty())
+	{
+		if (!failedCase)
+		{
 			failedCase = -1;
 		}
 
-		std::cout << size << " " << balancedTreeRef.countNodes() << std::endl;
-		balancedTreeRef.printVertical();
+		std::cout << size << " " << balancedTree.countNodes() << std::endl;
+		balancedTree.printVertical();
 		std::cout << std::endl;
 		std::cout << std::endl;
-
-
 	}
-	return (failedCase == 0);
+
+	return failedCase;
 }
 
 int main()
 {
 	srand(time(0));
-	std::cout << testBalancedSearchTree(30) << std::endl;
-	//std::cout << testBalancedAsSearchTree(30) << std::endl;
-	//std::cout << testBalancedAsBinaryTree(30) << std::endl;
+
+	std::cout << "----BALANCED_SEARCH_TREE----" << std::endl;
+	if (testBalancedSearchTree(30) == 0)
+	{
+		std::cout << "SUCCESS!" << std::endl;
+	}
+	else
+		std::cout << "OOPS HAVE SOME ERRORS!" << std::endl;
+	
+	std::cout << "\n\n\n----AS_SEARCH_TREE----" << std::endl;
+	if (testBalancedAsSearchTree(30) == 0)
+	{
+		std::cout << "SUCCESS!" << std::endl;
+	}
+	else
+		std::cout << "OOPS HAVE SOME ERRORS!" << std::endl;
+	
+	std::cout << "\n\n\n----AS_BINARY_TREE----" << std::endl;
+	if (testBalancedAsBinaryTree(30) == 0)
+	{
+		std::cout << "SUCCESS!" << std::endl;
+	}
+	else
+		std::cout << "OOPS HAVE SOME ERRORS!" << std::endl;
 
 	return 0;
 }
